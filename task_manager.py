6ede6ea6e49a8423e6f9adf29c1887f82dfe6c9b
TOVRH <vrhel.t@gmail.com>
2026-01-07 20:41:41 +0100

    osnova

---
+++ task_manager.py
@@ -0,0 +1,27 @@
+def hlavni_menu():
+    print("Správce úkolů- hlavní menu")
+    print("2. Zobrazit všehny úkoly")
+    print("3. Odstranit úkol")
+    print("4. Konec programu")
+    print("Vyberte možnost (1-4):")
+
+    ukoly =[]
+
+
+
+
+def pridat_ukol():
+    nazev = input("Zadejte název úkolu:")
+    popis =input("Zadejte popis úkolu:")
+
+    return print("úkol{nazev} byl přidán")
+
+
+
+
+def zobrazit_ukol():
+
+
+
+
+def odstranit_ukol():
